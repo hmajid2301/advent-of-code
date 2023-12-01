d667215c1e099d3b3cffe0f142a59a435f8c491e
Haseeb Majid <hello@haseebmajid.dev>
2023-12-01 19:27:01 +0000

    update todos

--- shell.nix
+++ shell.nix
@@ -17,24 +17,9 @@
 
 let
   goEnv = mkGoEnv { pwd = ./.; };
-  pre-commit-check = pre-commit-hooks.lib.${pkgs.system}.run {
-    src = ./.;
-    hooks = {
-      golangci-lint = {
-        enable = true;
-        name = "golangci-lint";
-        description = "Lint my golang code";
-        files = "\.go$";
-        entry = "task lint -- --new-from-rev HEAD --fix";
-        require_serial = true;
-        pass_filenames = false;
-      };
-    };
-  };
 in
 
 pkgs.mkShell {
-  inherit (pre-commit-check) shellHook;
   hardeningDisable = [ "all" ];
   packages = [
     goEnv
@@ -42,8 +27,6 @@ pkgs.mkShell {
     pkgs.golangci-lint
     pkgs.go_1_21
     pkgs.gotools
-    pkgs.go-junit-report
-    pkgs.go-task
     pkgs.delve
   ];
 }
