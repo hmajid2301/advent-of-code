bd5588affc5fd3ddb9ff715b3b4bed47f66fd3fa
Haseeb Majid <hello@haseebmajid.dev>
2023-12-02 16:22:58 +0000

    work

--- 2023/daytwo/solution.go
+++ 2023/daytwo/solution.go
@@ -20,7 +20,6 @@ func CalculatePossibleGames(results string) int {
 		gameNum := game[0]
 
 		num := strings.ReplaceAll(gameNum, "Game ", "")
-		fmt.Println("NUM", num)
 		num_, err := strconv.Atoi(num)
 		if err != nil {
 			panic(err)
@@ -61,6 +60,51 @@ func CalculatePossibleGames(results string) int {
 		if !impossible {
 			total += num_
 		}
+	}
+
+	return total
+}
+
+func CalculateMinimumCubes(results string) int {
+
+	lines := strings.Split(results, "\n")
+	var total int
+
+	for _, line := range lines {
+		game := strings.Split(line, ":")
+
+		minCubes := map[string]int{
+			"red":   0,
+			"blue":  0,
+			"green": 0,
+		}
+		turns := strings.Split(game[1], ";")
+		for _, turn := range turns {
+			cubes := strings.Split(turn, ",")
+			for _, cube := range cubes {
+				v, err := strconv.Atoi(strings.Split(cube, " ")[1])
+				if err != nil {
+					panic(err)
+				}
+
+				if strings.Contains(cube, "blue") {
+					if v > minCubes["blue"] {
+						minCubes["blue"] = v
+					}
+				} else if strings.Contains(cube, "red") {
+					if v > minCubes["red"] {
+						minCubes["red"] = v
+					}
+				} else if strings.Contains(cube, "green") {
+					if v > minCubes["green"] {
+						minCubes["green"] = v
+					}
+				}
+			}
+		}
+
+		total += minCubes["green"] * minCubes["red"] * minCubes["blue"]
+		fmt.Print("TOTOAL", total)
 	}
 
 	return total
