6f5c34041770e948b3d2d7a49120ac18df31fda7
Haseeb Majid <hello@haseebmajid.dev>
2023-12-08 21:09:20 +0000

    small fixes

--- 2023/dayeight/solution.go
+++ 2023/dayeight/solution.go
@@ -33,27 +33,50 @@ func CalculateStepsInRoute(tree string) int {
 func CalculateStepsToAllZ(tree string) int {
 	lines := strings.Split(tree, "\n")
 	treeMap := map[string][2]string{}
+	startNodes := []string{}
 	for _, line := range lines[2:] {
 		node := line[0:3]
 		left := line[7:10]
 		right := line[12:15]
 
 		treeMap[node] = [2]string{left, right}
+		if strings.HasSuffix(node, "A") {
+			startNodes = append(startNodes, node)
+		}
 	}
 
 	var dir = map[string]int{"L": 0, "R": 1}
 
+	stepsTaken := []int{}
 	path := lines[0]
-	var steps int
-	atNode := "AAA"
-	for atNode != "ZZZ" {
-		for _, route := range strings.Split(path, "") {
-			atNode = treeMap[atNode][dir[route]]
-			steps++
-			if atNode == "ZZZ" {
-				break
+	for _, node := range startNodes {
+		var steps int
+		atNode := node
+		for !strings.HasSuffix(atNode, "Z") {
+			for _, route := range strings.Split(path, "") {
+				atNode = treeMap[atNode][dir[route]]
+				steps++
 			}
 		}
+		stepsTaken = append(stepsTaken, steps)
 	}
-	return steps
+	return lcmN(stepsTaken)
+}
+
+func lcmN(n []int) int {
+	if len(n) == 2 {
+		return lcm(n[0], n[1])
+	}
+	return lcm(n[0], lcmN(n[1:]))
+}
+
+func lcm(a, b int) int {
+	return a * b / gcd(a, b)
+}
+
+func gcd(a, b int) int {
+	for b != 0 {
+		a, b = b, a%b
+	}
+	return a
 }
